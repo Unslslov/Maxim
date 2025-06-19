4dfc50824a42d3368fd5c82186dd81bd27b0ca02
CitricBlowFish <marat_khafizov@inbox.ru>
2025-06-19 09:41:51 +0300

    feat: добавлено восьмое задание

---
+++ c#_z8_webAPI/WithConfig.cs
@@ -0,0 +1,28 @@
+﻿namespace c__z8_webAPI
+{
+    public static class ForOtherFiles   // но здесь нигде нет проверки на то, есть ли соответствующте разделы в файле appsettings.json
+    {
+        private static IConfiguration _configuration;
+
+        public static void ForOtherFilesSetConfig(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public static string getAPI()
+        {
+            string urlAPI = _configuration["RandomAPI"];
+            return urlAPI;
+        }
+        public static string[] getBlackList()
+        {
+            string[] BlackList = _configuration.GetSection("Settings:BlackList").Get<string[]>();
+            if (BlackList == null)
+            {
+                BlackList = new string[0];
+                return BlackList;
+            }
+            return BlackList;
+        }
+    }
+}
